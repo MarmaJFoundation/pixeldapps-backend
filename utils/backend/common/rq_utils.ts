b06bac31d822717dc0f8bca9c4ce601a511c8e2c
NEAR Opensource <opensource@pixeldapps.co>
2022-11-16 00:20:04 +0100

    Fix error

--- utils/backend/common/rq_utils.ts
+++ utils/backend/common/rq_utils.ts
@@ -10,7 +10,7 @@ export function setup_headers(req: NextApiRequest, res: NextApiResponse): void {
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers");
     if (req.method === "OPTIONS") {
-        return res.status(200).end();
+        return res.status(200).json({});
     }
 }
 
